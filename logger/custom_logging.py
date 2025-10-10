d3edd818d15fa9164e09c14d89313699530c05ad
HoPhuKhang <fuskaqahihi@gmail.com>
2025-10-11 00:54:47 +0900

    custom logging file updated

---
+++ logger/custom_logging.py
@@ -0,0 +1,21 @@
+import os
+import logging
+from datetime import datetime
+
+class CustomLogger:
+    def __inti__(self, log_dir = "Logs"):
+        self.log_dir = os.path.join(os.getcwd(), log_dir)
+        os.makedirs(self.log_dir, exist_ok=True)
+        self.log_file = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+        self.log_file_path = os.path.join(self.log_dir, self.log_file)
+        logging.basicConfig(filename=self.log_file_path,
+                            format='[%(asctime)s] %(message)s',
+                            level=logging.INFO)
+    def get_logger(self, name=__file__):
+        return logging.getLogger(name)
+    
+if __name__ == "__main__":
+    logger = CustomLogger().get_logger(__file__)
+    logger.info("Logging has started")
+                            
+        
